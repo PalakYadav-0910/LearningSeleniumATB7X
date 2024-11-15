451f7f1414d8810dc5c1db2623dba77f8c9ba015
Palak_Yadav <palak.yadav-EXT@lebara.com>
2024-11-15 22:38:28 +0530

    Commit ex_02112024_Actions_Windows_Iframe

---
+++ src/test/java/org/example/ex_Selenium/ex_02112024_Actions_Windows_Iframe/Selenium45_WebTables_Dynamic.java
@@ -0,0 +1,60 @@
+package org.example.ex_Selenium.ex_02112024_Actions_Windows_Iframe;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Selenium45_WebTables_Dynamic {
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+    }
+
+    @Description("Verify Web Tables")
+    @Test
+    public void test_web_tables() throws InterruptedException {
+
+        driver.manage().window().maximize();
+        String URL = "https://awesomeqa.com/webtable1.html";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+        //Find the XPath for the WebTable
+        // //table[@summary = 'Sample Table']/tbody ->XPath
+        // table[summary = 'Sample Table']/tbody ->css
+
+        WebElement table = driver.findElement(By.xpath("//table[@summary = 'Sample Table']/tbody"));
+        //rows and columns
+        List<WebElement> rows_tables = table.findElements(By.tagName("tr"));
+        for(int i=0; i< rows_tables.size(); i++){
+            List<WebElement> col = rows_tables.get(i).findElements(By.tagName("td"));
+            for(WebElement c:col){
+                System.out.println(c.getText());
+            }
+        }
+
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
---
+++ src/test/java/org/example/ex_Selenium/ex_02112024_Actions_Windows_Iframe/Selenium47_Actions_P2.java
@@ -0,0 +1,53 @@
+package org.example.ex_Selenium.ex_02112024_Actions_Windows_Iframe;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Selenium47_Actions_P2 {
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--guest");
+        driver = new EdgeDriver(options);
+    }
+
+    @Description("Verify actions")
+    @Test
+    public void test_actions_p2() throws InterruptedException {
+
+        driver.manage().window().maximize();
+        String URL = "https://www.spicejet.com/";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+        WebElement source = driver.findElement(By.xpath("//div[text()='From']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(source).click().sendKeys("BLR").build().perform();
+        Thread.sleep(3000);
+
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+}
